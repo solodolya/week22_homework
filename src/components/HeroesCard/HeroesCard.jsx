8793fcc208d9ae9ee4b2b95db768ca9aca8bb80c
Olga <oa.solod@gmail.com>
2024-10-23 20:40:53 +1000

    Update HeroesCard

--- src/components/HeroesCard/HeroesCard.jsx
+++ src/components/HeroesCard/HeroesCard.jsx
@@ -4,14 +4,14 @@ import './HeroesCard.css';
 export default function HeroesCard({url, name, info, universe, alterego, occupation, friends, superpowers}) {
   return (
     <div className='card'>
-      <img src={url} className="card-img" alt={name}/>
-      <h2 className="card-title">{name}</h2>
-      <h4 className="card-info">{info}</h4>
-      <p className="card-text">Вселенная: {universe}</p>
-      <p className="card-text">Альтер эго: {alterego}</p>
-      <p className="card-text">Род деятельности: {occupation}</p>
-      <p className="card-text">Друзья: {occupation}</p>
-      <p className="card-text">Суперсилы: {superpowers}</p>
+      <img className="card-img" src={url} alt={name}/>
+      <h2 className="card-title">{name || "Не указано"}</h2>
+      <h4 className="card-info">{info || "Не указано"}</h4>
+      <p className="card-text">Вселенная: {universe || "Не указано"}</p>
+      <p className="card-text">Альтер эго: {alterego || "Не указано"}</p>
+      <p className="card-text">Род деятельности: {occupation || "Не указано"}</p>
+      <p className="card-text">Друзья: {friends || "Не указано"}</p>
+      <p className="card-text">Суперсилы: {superpowers || "Не указано"}</p>
     </div>
   );
 }
